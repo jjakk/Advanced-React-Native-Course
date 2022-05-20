d55c69e3c8136033590e1562efd925970bda71a2
John Khachian <jaksawesome10@gmail.com>
2022-05-19 20:16:16 -0400

    #9

--- swipe/src/Ball.js
+++ swipe/src/Ball.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component{
+    UNSAFE_componentWillMount(){
+        this.position = new Animated.ValueXY(0, 0);
+        Animated.spring(this.position, {
+            toValue: { x: 200, y: 500 },
+            useNativeDriver: false
+        }).start();
+    }
+
     render() {
         return (
-            <View style={styles.ball} />
+            <Animated.View style={this.position.getLayout()}>
+                <View style={styles.ball} />
+            </Animated.View>
         );
     }
 }
@@ -12,7 +22,7 @@ class Ball extends Component{
 const styles = {
     ball: {
         height: 60,
-        widthh: 60,
+        width: 60,
         borderRadius: 30,
         borderWidth: 30,
         borderColor: 'black',
